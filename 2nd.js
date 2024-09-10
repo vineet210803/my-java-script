65b75bcdb2696030291e18c7634bab1fb4baf1b0
vineet210803 <vineet210803@gmail.com>
2024-09-10 14:02:59 +0530

    my java script

---
+++ 2nd.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);  //simple math
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //here == & comparison(<,>) operator works differently, comparison converts null to number treating as 0.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+// console.log("2" === 2);
